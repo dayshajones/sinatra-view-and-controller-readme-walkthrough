1aab18a7027561b597f7b2f2892e205b24c68dcc
“Daysha <+>
2021-06-28 15:47:08 -0700

    commit tree 376e2bd3fa919c3f7e60e81569c6f46143040386

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+    original_string = params["string"]
+  @reversed_string = original_string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
-
+    @friends = ['Papa Smurf', 'Captain Marvel', 'Wonder Woman', 'Superman', 'Micky Mouse', 'Malala Yousafzai']
+    erb :friends
   end
 end
